854e951ed5f1f43b764f796d341cca8184a90dce
你的名字 <>
2025-03-16 15:58:39 +0800

    初始版本：腊肠狗吃鱼油游戏

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'dachshund-game-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './game.js',
+    './manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('缓存已打开');
+                return cache.addAll(ASSETS);
+            })
+    );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 拦截请求并从缓存中响应
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    (response) => {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
+    );
+}); 
